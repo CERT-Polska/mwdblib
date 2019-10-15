5ca822e4c290256058141a6945eca2b8be04f62a
msm <msm@cert.pl>
2019-10-15 16:14:20 +0200

    Update the API

--- src/__init__.py
+++ src/__init__.py
@@ -4,14 +4,16 @@ from .file import MalwarecageFile
 from .object import MalwarecageObject
 from .config import MalwarecageConfig
 from .blob import MalwarecageBlob
+from .util import config_dhash
 
 __all__ = [
-    'Malwarecage',
-    'MalwarecageAPI',
-    'MalwarecageFile',
-    'MalwarecageObject',
-    'MalwarecageConfig',
-    'MalwarecageBlob',
+    "Malwarecage",
+    "MalwarecageAPI",
+    "MalwarecageFile",
+    "MalwarecageObject",
+    "MalwarecageConfig",
+    "MalwarecageBlob",
+    "config_dhash",
 ]
 
 __version__ = "3.0.0"
