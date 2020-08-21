e426e3d9069d61a77dab661a4026e2e6620d12d7
Pawel Srokosz <pawel.srokosz@cert.pl>
2020-08-21 13:05:46 +0200

    Added support for 'in-blob' in util.config_dhash

--- src/util.py
+++ src/util.py
@@ -12,18 +12,39 @@ def convert_to_utf8(obj):
     return obj
 
 
-def config_dhash(obj):
+def _eval_config_dhash(obj):
     """ Compute a data hash from the object. This is the hashing algorithm
     used internally by malwarecage to assign unique ids to configs
     """
     if isinstance(obj, list):
         # For lists: evaluate hash recursively for all elements and sort them lexicographically
-        return config_dhash(str(sorted([config_dhash(o) for o in obj])))
+        return _eval_config_dhash(str(sorted([_eval_config_dhash(o) for o in obj])))
     elif isinstance(obj, dict):
         # For dicts: convert to key-ordered tuples with hashed value
-        return config_dhash(
-            [[o, config_dhash(obj[o])] for o in sorted(obj.keys())]
+        return _eval_config_dhash(
+            [[o, _eval_config_dhash(obj[o])] for o in sorted(obj.keys())]
         )
     else:
         # Other types: evaluate SHA256 after conversion to UTF-8
         return hashlib.sha256(convert_to_utf8(obj)).hexdigest()
+
+
+def config_dhash(obj):
+    """
+    Compute a data hash from the object. This is the hashing algorithm
+    used internally by malwarecage to assign unique ids to configs.
+
+    .. versionchanged:: 3.3.0
+        Added support for in-blob keys
+
+    :param obj: Dict with configuration
+    :type obj: dict
+    :return: SHA256 hex digest
+    """
+    config = dict(obj)
+    for key, value in config.items():
+        if isinstance(value, dict) and list(value.keys()) == ["in-blob"]:
+            in_blob = value["in-blob"]
+            if isinstance(in_blob, dict):
+                config[key]["in-blob"] = hashlib.sha256(convert_to_utf8(in_blob["content"])).hexdigest()
+    return _eval_config_dhash(config)
