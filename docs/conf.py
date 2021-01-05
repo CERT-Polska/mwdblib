70b4a2260d763d084be30e66bdb9cf9373b800fd
Paweł Srokosz <pawel.srokosz@cert.pl>
2021-01-05 16:01:02 +0100

    Bump version: 3.4.0 (#23)

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ from src import __version__  # noqa: E402
 # -- Project information -----------------------------------------------------
 
 project = 'mwdblib'
-copyright = '2020, CERT Polska'
+copyright = '2021, CERT Polska'
 author = 'CERT Polska'
 
 # The full version, including alpha/beta/rc tags
