a5777f641baab76409185aa499b419be60a8b3f8
msm-code <msm@tailcall.net>
2023-01-10 13:58:26 +0100

    Add remove_comment function to object (#87)

--- mwdblib/object.py
+++ mwdblib/object.py
@@ -387,6 +387,16 @@ class MWDBObject(MWDBElement):
         )
         self._expire("attributes")
 
+    def remove_comment(self, comment_id: int) -> None:
+        """
+        Remove specific comment from object
+
+        :param comment_id: Comment id
+        :type comment_id: int
+        """
+        self.api.delete(f"object/{self.id}/comment/{comment_id}")
+        self._expire("comments")
+
     @add_attribute.fallback("2.0.0")
     def _add_attribute_fallback(self, key: str, value: str) -> None:
         self._add_metakey(key, value)
